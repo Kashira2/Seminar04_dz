c74ef8852ae66ba703be76d2af4af4c2947c03d4
Kashira2 <nekit.egorov@icloud.com>
2022-08-30 15:02:51 +0300

    dz1

---
+++ dz1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите два числа: ");
+int x1 = int.Parse(Console.ReadLine());
+int x2 = int.Parse(Console.ReadLine());
+double Degree(int FirstNum, int SecondNum)
+{
+double result = Math.Pow(FirstNum, SecondNum);
+return result;
+}
+Console.WriteLine(Degree(x1, x2));
