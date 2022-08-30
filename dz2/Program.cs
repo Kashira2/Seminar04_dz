aed80d0fe0db5ba787489b146d5f04b5a6713603
Kashira2 <nekit.egorov@icloud.com>
2022-08-30 15:27:09 +0300

    dz2

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Sum(int number)
+{
+int result = 0;
+while (number >= 1)
+{
+    int LastNum = number % 10;
+    result = result + LastNum;
+    number = number / 10;
+}
+return result;
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine(Sum(num));
--- dz3/Program.cs
+++ dz3/Program.cs
@@ -1,15 +1,2 @@
-﻿int Sum(int number)
-{
-int result = 0;
-while (number >= 1)
-{
-    int LastNum = number % 10;
-    result = result + LastNum;
-    number = number / 10;
-}
-return result;
-}
-
-Console.WriteLine("Введите число: ");
-int num = int.Parse(Console.ReadLine());
-Console.WriteLine(Sum(num));
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
